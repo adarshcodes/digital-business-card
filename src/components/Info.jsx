b5287184dbbecc9a9f811cbc043b59269957178f
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-27 14:05:37 +0530

    completed info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "../App.css";
-import profile from "../images/profile.jpg";
 
 class Info extends React.Component {
 	render() {
 		return (
 			<div className="info-content">
-				<div className="profile-picture">
-					<img src={profile} alt="profile-picture" />
-				</div>
+				<div className="profile-picture"></div>
 				<div className="profile-details">
-					<h3 className="profile-name">Adarsh Pratap Singh</h3>
+					<h3 className="profile-name">Adarsh Pratap</h3>
 					<h4 className="role">Frontend Developer</h4>
 					<p className="site">
 						<a href="https://github.com/adarshcodes">officiallyAdarsh.com</a>
@@ -18,13 +15,13 @@ class Info extends React.Component {
 				</div>
 
 				<div className="contact">
-					<div className="contact-button">
-						<i class="fad fa-envelope"></i>
+					<div className="contact-button button-light">
+						<i class="fas fa-envelope"></i>
 						<p>
 							<a href="mailto:adarshpratapofficial@gmail.com">Email</a>
 						</p>
 					</div>
-					<div className="contact-button">
+					<div className="contact-button button-dark">
 						<i class="fab fa-linkedin"></i>
 						<p>
 							<a href="https://www.linkedin.com/in/adarsh-pratap-singh-338928194/">
