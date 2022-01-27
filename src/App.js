c0ce06c8a615149b31311785f77023b6ed5a7c0d
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-27 14:54:37 +0530

    fixed footer padding

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 			<Info />
 			<About />
 			<Interest />
-			{/*<Footer /> */}
+			<Footer />
 		</div>
 	);
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,35 @@
 import React from "react";
 import "../App.css";
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer className="footer">
+				<ul className="footer-icon-list">
+					<li className="footer-icon">
+						<a href="https://twitter.com/ideallyadarsh">
+							<i class="fab fa-twitter-square"></i>
+						</a>
+					</li>
+					<li className="footer-icon">
+						<a href="#">
+							<i class="fab fa-facebook-square"></i>
+						</a>
+					</li>
+					<li className="footer-icon">
+						<a href="https://www.instagram.com/officiallyadarsh/">
+							<i class="fab fa-instagram-square"></i>
+						</a>
+					</li>
+					<li className="footer-icon">
+						<a href="https://www.github.com/adarshcodes">
+							<i class="fab fa-github-square"></i>
+						</a>
+					</li>
+				</ul>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
